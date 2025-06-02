981ea5e51b6dea036d35043e8440131ca0761d9f
Oscar Javier <oscarjavierdos@gmail.com>
2025-06-02 18:55:28 +0200

    Refactor calculadora de hora de salida: mejora en la estructura HTML, estilos CSS y lógica de cálculo en JavaScript.

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-function calculateExitTime() {
-    const entryTimeInput = document.getElementById('entry-time');
-    const resultDisplay = document.getElementById('result');
+function calcularSalida() {
+    debugger
+    let horaEntrada = parseInt(document.getElementById("horaEntrada").value);
+    let minutosEntrada = parseInt(document.getElementById("minutosEntrada").value);
+    console.log('Entrada H/m',horaEntrada,'/',minutosEntrada);
 
-    const entryTime = new Date(`1970-01-01T${entryTimeInput.value}:00`);
-    const exitTime = new Date(entryTime.getTime() + 8 * 60 * 60 * 1000); // Adding 8 hours
+    let horaSalida = parseInt(document.getElementById("horaSalida").value);
+    let minutosSalida = parseInt(document.getElementById("minutosSalida").value);
+    console.log('Salida H/m',horaSalida,'/',minutosSalida);
 
-    const options = { hour: '2-digit', minute: '2-digit', hour12: false };
-    resultDisplay.textContent = `Hora de salida: ${exitTime.toLocaleTimeString('en-US', options)}`;
+
+    let tiempoComida =60- parseInt(document.getElementById("tiempoComida").value);
+    console.log('Comida',tiempoComida);
+
+    if (!isNaN(horaEntrada) && !isNaN(minutosEntrada) && !isNaN(tiempoComida)) {
+        let salidaPrevista = new Date();
+        salidaPrevista.setHours(horaSalida);
+        salidaPrevista.setMinutes(minutosSalida - tiempoComida);
+
+        let salidaFinal = salidaPrevista.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        console.log('salida',salidaFinal);
+        document.getElementById("resultado").innerText = "Hora final de salida: " + salidaFinal;
+    } else {
+        document.getElementById("resultado").innerText = "Por favor, ingresa todos los datos correctamente.";
+    }
 }
-
-document.getElementById('calculate-button').addEventListener('click', calculateExitTime);
+calcularSalida();
